fec6040e4f44aba9b5951def70a1b208df0a670d
Abdul Hannan Kanji <akanji@umass.edu>
2020-08-06 12:31:18 -0400

    Add utils

--- code/utils/_utils.py
+++ code/utils/_utils.py
@@ -8,6 +8,10 @@ def get_root():
     root_path = Path(os.path.dirname(__file__)) / '..' / '..'
     return root_path.resolve()
 
+def get_data():
+    root = get_root()
+    return root/ 'data-processed'
+
 def get_forecasts():
     root = get_root()
     forecasts = root.glob('data-processed/**/*.csv')
@@ -17,3 +21,8 @@ def get_models():
     root = get_root()
     models = [f.name for f in (root / 'data-processed').iterdir()]
     return models
+
+def get_metadata(model=None):
+    data = get_data()
+    if model is not None:
+        pass
